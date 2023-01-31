5c495cc0e26346f779f38c718bea23ccc7fc9426
Samia Khan Trisha <121845776+SamiaKhanTrisha@users.noreply.github.com>
2023-01-31 11:33:27 +0600

    Add files via upload

---
+++ LAB - 03/bit.cpp
@@ -0,0 +1,29 @@
+//Problem name-Bit++
+//Problem no-282A
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int a,x=0;
+    string s;
+    cin>>a;
+    for (int i=0;i<a;i++)
+    {
+        cin>>s;
+        if (s[1]=='+')
+        {
+            x++;
+        }
+        else
+        {
+            x--;
+        }
+    }
+    cout<<x<<endl;
+    return 0;
+}
+
+
+
